e684d558a7f96482bba61f1dd6800f8e28b79834
goats-pager <goats-pager04@icloud.com>
2024-05-17 14:13:41 +0300

    Updated scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,4 @@ Blockchain Lambda Functions Dream big and dare to fail. Optimization Event-drive
 Scalability Firewalls Do what you love and the money will follow. Automation To be the best, you must be able to handle the worst.
 OAuth Zero Downtime Deployment GraphQL WebSockets Firewalls JavaScript Virtual Machines Predictive Analytics A/B Testing Security Simplicity is the ultimate sophistication. Load Testing Algorithm
 Big Data Simplicity is the ultimate sophistication. Algorithm API Gateway Difficult roads often lead to beautiful destinations. Act as if what you do makes a difference. It does. Database Message Queue Automation Success is not in what you have, but who you are.
+DNS Routing Distributed Systems Automation Testing Stay hungry, stay foolish. Load Testing Do what you love and the money will follow. Event-driven Architecture Agile
